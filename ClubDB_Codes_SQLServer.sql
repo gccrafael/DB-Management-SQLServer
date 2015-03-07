02b2942e3294ca63ad6f2746167f6341a5153f78
gccrafael <gccrafael@gmail.com>
2015-03-07 11:32:04 -0600

    db sqlserver
    db sqlserver

---
+++ ClubDB_Codes_SQLServer.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tblCodes (
+   TransCd VARCHAR(2) NOT NULL,
+   TransDesc VARCHAR(50)
+);
+
+INSERT INTO tblCodes VALUES ('00','Payment');
+INSERT INTO tblCodes VALUES ('01','Dues');
+INSERT INTO tblCodes VALUES ('02','Food');
+INSERT INTO tblCodes VALUES ('03','Bar');
+INSERT INTO tblCodes VALUES ('21','Sports Bar - Food');
+INSERT INTO tblCodes VALUES ('22','Sports Bar - Drink');
+INSERT INTO tblCodes VALUES ('25','Retail Purchase (Novelty)');
+INSERT INTO tblCodes VALUES ('EF','Entertainment Fee');
+INSERT INTO tblCodes VALUES ('FR','Facility Rent - Club');
+INSERT INTO tblCodes VALUES ('LF','Late Fee');
+INSERT INTO tblCodes VALUES ('NS','NoShow');
+INSERT INTO tblCodes VALUES ('TK','Event Tickets');
+INSERT INTO tblCodes VALUES ('VP','Valet Parking');
---
+++ ClubDB_Members_SQLServer.sql
@@ -0,0 +1,38 @@
+CREATE TABLE tblMembers (
+  MemID VARCHAR(4) NOT NULL,
+  LastName VARCHAR(30),
+  FirstName VARCHAR(30),
+  MiddleName VARCHAR(15),
+  MemStatus VARCHAR(1),
+  MemDt DATETIME,
+  YTD_Total FLOAT,
+  YTD_Total_Dt FLOAT,
+  MemPassword INT
+);
+
+INSERT INTO tblMembers VALUES ('A043','Abraham','Joseph','G.','C','2008-03-10',Null,Null,18);
+INSERT INTO tblMembers VALUES ('B146','Boyle','John','W.','C','2008-02-17',Null,Null,29);
+INSERT INTO tblMembers VALUES ('C099','Crisp II','Harry','L.','C','2008-01-01',Null,Null,40);
+INSERT INTO tblMembers VALUES ('C355','Creech','Thomas','E.','C','2008-03-27',Null,Null,51);
+INSERT INTO tblMembers VALUES ('D053','Dougherty','James','A.','C','2008-02-18',Null,Null,62);
+INSERT INTO tblMembers VALUES ('D110','DeFosset','Dan','D.','C','2008-02-23',Null,Null,73);
+INSERT INTO tblMembers VALUES ('E211','Ekvall','Daniel','F.','C','2010-02-22',Null,Null,84);
+INSERT INTO tblMembers VALUES ('F200','Frierdich','Paul','P.','C','2008-01-01',Null,Null,95);
+INSERT INTO tblMembers VALUES ('G116','Goldwasser','Robert','A.','C','2008-03-02',Null,Null,106);
+INSERT INTO tblMembers VALUES ('G305','Gibson','Dan','T.','C','2009-03-26',Null,Null,117);
+INSERT INTO tblMembers VALUES ('H019','Harrison','William','John','C','2008-03-17',Null,Null,128);
+INSERT INTO tblMembers VALUES ('J061','Johnson','Don','Edwin','C','2008-01-01',Null,Null,139);
+INSERT INTO tblMembers VALUES ('K132','Kutis III','Thomas','Frank','C','2008-02-20',Null,Null,150);
+INSERT INTO tblMembers VALUES ('L105','Lizenby','Chuck','G.','C','2008-03-04',Null,Null,161);
+INSERT INTO tblMembers VALUES ('M111','Marks','William','E.','C','2008-02-18',Null,Null,172);
+INSERT INTO tblMembers VALUES ('P109','Phillips','David','H.','C','2008-02-17',Null,Null,183);
+INSERT INTO tblMembers VALUES ('S511','Schwadron','Richard','A.','C','2008-02-19',Null,Null,194);
+INSERT INTO tblMembers VALUES ('S526','Squires','Bradley','S.','C','2008-02-17',Null,Null,205);
+INSERT INTO tblMembers VALUES ('T187','Tarpinian','Gary','C.','C','2010-04-07',Null,Null,216);
+INSERT INTO tblMembers VALUES ('U014','Uline','Steve','T.','C','2008-02-20',Null,Null,227);
+INSERT INTO tblMembers VALUES ('V063','Viola','Mark','P.','C','2008-02-23',Null,Null,238);
+INSERT INTO tblMembers VALUES ('W147','Warmann','Floyd','C.','C','2008-01-01',Null,Null,249);
+INSERT INTO tblMembers VALUES ('W155','Wilson','Frederick','T.','C','2008-03-10',Null,Null,260);
+INSERT INTO tblMembers VALUES ('X770','Huber','Richard','K.','C','1996-04-08',Null,Null,271);
+INSERT INTO tblMembers VALUES ('Y027','Young','John','C.','C','2008-02-24',Null,Null,282);
+INSERT INTO tblMembers VALUES ('Z005','Zimmerman','Carl','L.','C','2008-02-18',Null,Null,293);
